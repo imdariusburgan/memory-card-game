bc9dfd4022cc4a323aa0445832f00e2c26d8c8ce
Darius Burgan <darius@dariusburgan.com>
2018-05-20 01:11:08 -0400

    Selected important elements, Added launch game function and prepareGame

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,29 @@
 
-const prepareGame = () => {
+// Select Div Container
+const divContainer = document.querySelector(".container");
+
+// Select 'Moves' counter
+const movesCounter = document.querySelector(".moves");
+
+// An empty list to store clicked cards for match comparison
+let cardCompareList = [];
+
+const launchGame = () => {
+
+    prepareGame();
 
 }
+
+const prepareGame = () => {
+    // Create game table/deck
+    const createCardDeck = document.createElement("ul");
+    divContainer.appendChild(createCardDeck).className = "deck";
+
+    // Select game table/deck
+    const cardDeck = document.querySelector(".deck");
+
+    // Clear/Reset the current card layout
+    cardDeck.innerHTML = "";
+}
+
+launchGame();
