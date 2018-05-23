ccc254a4c6c50104cfb4e94f9dac28893312bee8
Darius Burgan <darius@dariusburgan.com>
2018-05-22 22:04:38 -0400

    Replaced " " with ' '

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@
 
 
 // Select Div Container
-const divContainer = document.querySelector(".container");
+const divContainer = document.querySelector('.container');
 
 // Select 'Moves' counter
-const movesCounter = document.querySelector(".moves");
+const movesCounter = document.querySelector('.moves');
 
 // An empty list to store clicked cards for match comparison
 let cardCompareList = [];
@@ -17,20 +17,20 @@ let cardCompareList = [];
 const restartButton = document.querySelector('.restart');
 
 // Different card types
-const diamond = "diamond";
-const paperPlane = "paper-plane-o";
-const anchor = "anchor";
-const bolt = "bolt";
-const cube = "cube";
-const leaf = "leaf";
-const bicycle = "bicycle";
-const bomb = "bomb";
+const diamond = 'diamond';
+const paperPlane = 'paper-plane-o';
+const anchor = 'anchor';
+const bolt = 'bolt';
+const cube = 'cube';
+const leaf = 'leaf';
+const bicycle = 'bicycle';
+const bomb = 'bomb';
 
 // An array of all cards types in deck
 const cardsInDeck = [diamond, diamond, paperPlane, paperPlane, anchor, anchor, bolt, bolt, cube, cube, leaf, leaf, bicycle, bicycle, bomb, bomb];
 
 // When user clicks the button to restart the game, the restartGame function will run
-restartButton.addEventListener("click", (event) => {
+restartButton.addEventListener('click', (event) => {
     restartGame();
 });
 
@@ -47,7 +47,7 @@ const countTimer = () => {
     const minute = Math.floor((totalSeconds - hour*3600)/60);
     const seconds = totalSeconds - (hour*3600 + minute*60);
 
-    document.querySelector(".time").innerHTML = `${minute} minute(s) ${seconds} second(s)`;
+    document.querySelector('.time').innerHTML = `${minute} minute(s) ${seconds} second(s)`;
 }
 // Variables for starting and stopping the timer
 let timerVar = setInterval(countTimer, 1000);
@@ -72,8 +72,8 @@ function shuffle(array) {
 const starRating = () => {
 
     // Select the list that holds the stars
-    const starsList = document.querySelector(".stars")
-    const starIcon = document.querySelector(".fa-star");
+    const starsList = document.querySelector('.stars')
+    const starIcon = document.querySelector('.fa-star');
     /*
      * If number of moves equal 10, the player will be bumped down to 2 stars
      * If number of moves equal 15, the player will be bumped down to 1 stars
@@ -91,7 +91,7 @@ const starRating = () => {
 const doCardsMatch = () => {
     const blankCards = document.querySelectorAll('.card');
     blankCards.forEach( (card) => {
-        card.addEventListener("click", (event) => {
+        card.addEventListener('click', (event) => {
             if (card.classList.contains('show') || card.classList.contains('match') ) {
                 null;
             } else {
@@ -101,7 +101,7 @@ const doCardsMatch = () => {
 
             if (cardCompareList.length == 2) {
 
-                const clickedCards = document.querySelectorAll(".open");
+                const clickedCards = document.querySelectorAll('.open');
 
                 if (cardCompareList[0] == cardCompareList[1] ) {
                     correctlyMatched(clickedCards);
@@ -149,19 +149,19 @@ const restartGame = () => {
 const buildDeck = () => {
 
     // Create game table/deck and append to page
-    const createCardDeck = document.createElement("ul");
-    divContainer.appendChild(createCardDeck).className = "deck";
+    const createCardDeck = document.createElement('ul');
+    divContainer.appendChild(createCardDeck).className = 'deck';
 
     // Select game table/deck
-    const cardDeck = document.querySelector(".deck");
+    const cardDeck = document.querySelector('.deck');
 
     // Clear/Reset the current card layout
-    cardDeck.innerHTML = "";
+    cardDeck.innerHTML = '';
 
     // Create 16 blank cards for game table/deck
     for (let i = 0; i < 16; i++) {
         let blankCard = document.createElement('li');
-        cardDeck.appendChild(blankCard).className = "card";
+        cardDeck.appendChild(blankCard).className = 'card';
     }
 
     /*
@@ -172,7 +172,7 @@ const buildDeck = () => {
     const blankCards = document.querySelectorAll('.card');
     blankCards.forEach( (card) => {
         let uniqueCard = document.createElement('i');
-        card.appendChild(uniqueCard).className = "fa fa-";
+        card.appendChild(uniqueCard).className = 'fa fa-';
     });
 
     // grab all 'i' elements
@@ -198,7 +198,7 @@ const resetTimer = () => {
     clearInterval(timerVar);
 
     // Reset timer
-    document.querySelector(".time").innerHTML = " ";
+    document.querySelector('.time').innerHTML = '';
 
     // Start new timer
     totalSeconds = 0;
@@ -219,7 +219,7 @@ const resetMovesAndStars = () => {
     // Creates stars ul element
     const newStars = document.createElement('ul')
     scorePanel.insertBefore(newStars, document.querySelector('.moves'));
-    document.querySelector('.score-panel ul').className = "stars";
+    document.querySelector('.score-panel ul').className = 'stars';
 
     // Creates star icons
     for (let i = 0; i < 3; i++) {
@@ -239,14 +239,14 @@ const resetMovesAndStars = () => {
         document.querySelectorAll('.stars li')[i].appendChild(newI);
 
         // Adds a class to the newly appended i element
-        document.querySelectorAll('.stars li i')[i].className = "fa fa-star";
+        document.querySelectorAll('.stars li i')[i].className = 'fa fa-star';
     }
 
 }
 
 // This function displays a card's symbol by adding a class to it
 const showCardSymbol = (card) => {
-    card.classList.add("open", "show");
+    card.classList.add('open', 'show');
 }
 
 // Function to move any card that's clicked to a list of open cards in order to compare for matches.
@@ -259,7 +259,7 @@ const inCorrectlyMatched = (clickedCards) => {
     setTimeout( () => {
         clickedCards.forEach( (symbol) => {
             // remove the class 'open' and 'show' to flip the card back over.
-            symbol.classList.remove("open", "show");
+            symbol.classList.remove('open', 'show');
         });
 
         // clear the array holding the 2 cards being compared
@@ -274,8 +274,8 @@ const correctlyMatched = (clickedCards) => {
     // Add the class 'match' to lock the cards in a position showing their symbols.
     // Remove the class 'open' and 'show' because it's no longer needed.
     clickedCards.forEach( (matchingCard) => {
-        matchingCard.classList.add("match");
-        matchingCard.classList.remove("open", "show");
+        matchingCard.classList.add('match');
+        matchingCard.classList.remove('open', 'show');
     });
 
     // clear the array holding the 2 cards being compared.
@@ -293,7 +293,7 @@ const increaseMoves = () => {
 const congrats = () => {
     if (document.querySelectorAll('.match').length === 16) {
         clearInterval(timerVar);
-        alert(`Congratulations! It took you ${document.querySelector(".time").innerHTML} seconds and ${movesCounter.innerHTML} moves to win!`)
+        alert(`Congratulations! It took you ${document.querySelector('.time').innerHTML} seconds and ${movesCounter.innerHTML} moves to win!`)
     }
 };
 
